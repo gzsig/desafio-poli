ee60a1f97779a7a59c1bfb5bf69833bbf80adf39
Gabriel Zsigmond <gaazsig@gmail.com>
2019-06-22 16:43:20 -0300

    easier for user to enter values added error msg

--- print_matrix.py
+++ print_matrix.py
@@ -1,30 +1,50 @@
-# A basic code for matrix input from user 
-
 M = int(input("Enter the size of your square matrix:")) 
 
 # Initialize matrix 
 matrix = [] 
-print("Enter the entries rowwise:") 
+numbers = []
+one_liner = []
+valid = True
 
-numbers = input()
-numbers = numbers.split()
-if (len(numbers) / M ) != M:
-  print("wrong number of arguments")
+while valid:
+  print("Enter the entries for the 1 row:") 
+  # For user input 
+  first_row = (input())
+  first_row = first_row.split()
+  if len(first_row) != M:
+    print("Wrong number of arguments, given {} expected {}".format(len(first_row), M))
+    valid = False
+  if valid:
+    numbers.append(first_row)
+    for i in range(M-1):
+      print("Enter the entries for the {} row:".format(str(i+2)))
+      new_row = (input())
+      new_row = new_row.split()
+      numbers.append(new_row)
+    # create an array with with all numbers
+    for row in numbers:
+      for number in row:
+        one_liner.append(number)
+    # print(one_liner)
 
-cont = 0
+    # if (len(one_liner) / M ) != M:
+    #   print("wrong number of arguments")
 
-# For user input 
-for i in range(M):     # A for loop for row entries 
-  a =[] 
-  for j in range(M):   # A for loop for column entries 
-    a.append(numbers[j + (cont*M)]) 
-  matrix.append(a) 
-  cont += 1
-  
+    cont = 0
 
-# For printing the matrix 
-print("Entry matrix:")
-for i in range(M): 
-  for j in range(M): 
-    print(matrix[i][j], end = " ") 
-  print() 
+
+    for i in range(M):     # A for loop for row entries 
+      a =[] 
+      for j in range(M):   # A for loop for column entries 
+        a.append(one_liner[j + (cont*M)]) 
+      matrix.append(a) 
+      cont += 1
+      
+
+    # For printing the matrix 
+    print("Entry matrix:")
+    for i in range(M): 
+      for j in range(M): 
+        print(matrix[i][j], end = " ") 
+      print() 
+  valid =False
